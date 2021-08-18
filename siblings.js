cffd61656aa89a0620cee7a20a07120c037c4f95
Sergio D. Barrera <sdbeng@gmail.com>
2021-08-18 13:28:59 -0700

    siblings nodes

---
+++ siblings.js
@@ -0,0 +1,17 @@
+const Node = require('./main')
+
+const oldest = new Node('John');
+const middle = new Node('Jacob');
+const youngest = new Node('Jingleheimer');
+ 
+youngest.setNextNode(middle);
+middle.setNextNode(oldest);
+ 
+let currentSibling = youngest;
+let oldestName = '';
+while(currentSibling !== null) {
+  oldestName= currentSibling.data;
+  currentSibling= currentSibling.getNextNode();
+}
+ 
+console.log(`There goes ${oldestName} Schmidt!`);
